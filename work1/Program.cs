c54299152bd545bb1c37a20920ab36f37ce3a544
Elena <lenalosyakova@gmail.com>
2022-11-12 20:46:33 +0300

    натур.числы от M до N

---
+++ work1/Program.cs
@@ -0,0 +1,19 @@
+﻿// показать натуральные числа от М до N, N и М заданы
+Console.WriteLine("Введите число (M): ");
+int M = int.Parse(Console.ReadLine() ?? "0");
+Console.WriteLine("Введите число (N): ");
+int N = int.Parse(Console.ReadLine() ?? "0");
+if (M > N)
+{
+    Console.Write($"Невозможно, т.к. N должно быть больше M");
+    return;
+}
+void Integers(int k, int l)
+{
+    if (k < l)
+    {
+        Console.Write(k + " ");
+        Integers(k + 1, l);
+    }
+}
+Integers(M + 1, N);
